9e2d0dc0c36b33f8e8fa726c0d7523fd23dc06a3
onifade-joshua <onifadejoshua15@gmail.com>
2023-09-11 15:07:58 +0100

    Add project files.

---
+++ Example2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            var TotalNumber = 100;
+            var GuessMyNumber = Convert.ToInt16(Console.ReadLine());
+           ;
+           
+            if(GuessMyNumber == 0)
+            {
+                Console.WriteLine("You guessed zero, which is not allowed.");
+            }
+            else
+            {
+                //var ActualGuess = TotalNumber - GuessMyNumber;
+                if(GuessMyNumber <= TotalNumber)
+                {
+                    Console.WriteLine("Yo! your number is lesser...");
+                }
+                else
+                {
+                    Console.WriteLine("Hey!, your number is higher!!!");
+                }
+                
+            }
+            Console.WriteLine("Pls press a key to continue...");
+            Console.ReadKey();
+          
+        }
+    }
+}
